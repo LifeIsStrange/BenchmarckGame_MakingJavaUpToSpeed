d1030378648bfce431522322a0cfc876c559a38a
LifeIsStrange <steph.dubedat@gmail.com>
2021-03-24 01:38:19 +0000

    optimize the impl by moving tmp array allocation out of the loop

--- spectralNorm/src/main/java/SpectralNorm.java
+++ spectralNorm/src/main/java/SpectralNorm.java
@@ -9,12 +9,17 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 import java.util.Vector;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import static jdk.incubator.vector.VectorOperators.*;
 class SpectralNorm
 {
     static final VectorSpecies<Double> SPECIES = DoubleVector.SPECIES_128;
+    static final ExecutorService pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
 
     public static void main(String[] args)
     {
@@ -35,14 +40,17 @@ class SpectralNorm
             mult_AtAv(u, v, n);
             mult_AtAv(v, u, n);
         }
+        pool.shutdown();
+        while (!pool.isTerminated()) {}
+
+
 
         // WHY NO CLONING despite span value type
         var result = Math.sqrt(dot(u, v, n) / dot(v, v, n));
         System.out.println(BigDecimal.valueOf(result).setScale(9, RoundingMode.HALF_UP));
 
-        var endTime = Instant.now();
-        var duration = Duration.between(startTime, endTime);
-        System.out.println(duration);
+        Instant endTime = Instant.now();
+        System.out.println(Duration.between(startTime, endTime));
     }
 
     private static double A(int i, int j)
@@ -60,83 +68,80 @@ class SpectralNorm
 
     private static void mult_Av(double[] v, double[] outv, int n)
     {
-        var list = new int[n];
-
-        // make the array ascending
-        for (int i = 0; i < n ; i++) {
-            list[i] = i;
-        }
-
         // bench without and with a threadpool
-        Arrays.stream(list).parallel().forEach( i -> {
-            var sum = DoubleVector.zero(SPECIES);
 
-            for (var j = 0; j < n; j += 2)
-            {
-                var b = DoubleVector.fromArray(SPECIES, v, j);
-                // needless array
+        IntStream.range(0, n).forEach(i -> {
+            pool.execute(() -> {
                 var tmp = new double[2];
-                tmp[0] = A(i, j);
-                tmp[1] = A(i, j + 1);
-                var a = DoubleVector.fromArray(SPECIES, tmp, 0);
 
-                sum = sum.add(b.div(a));
-            }
+                var sum = DoubleVector.zero(SPECIES);
 
-            // should be horizontal
-            // explanation of horizontal add https://www.quora.com/Is-there-an-SIMD-architecture-that-supports-horizontal-cumulative-sum-Prefix-sum-as-a-single-instruction
-            var mask = new boolean[2];
-            Arrays.fill(mask, true);
-            var r = sum.reduceLanes(ADD, VectorMask.fromArray(SPECIES,mask, 0));
+                for (var j = 0; j < n; j += 2)
+                {
+                    var b = DoubleVector.fromArray(SPECIES, v, j);
+                    // needless array
+                    tmp[0] = A(i, j);
+                    tmp[1] = A(i, j + 1);
+                    var a = DoubleVector.fromArray(SPECIES, tmp, 0);
 
-            ///sum = sum.add(sum);
+                    sum = sum.add(b.div(a));
+                }
+
+                // should be horizontal
+                // explanation of horizontal add https://www.quora.com/Is-there-an-SIMD-architecture-that-supports-horizontal-cumulative-sum-Prefix-sum-as-a-single-instruction
+                //var mask = new boolean[2];
+                //Arrays.fill(mask, true);
+                //var r = sum.reduceLanes(ADD, VectorMask.fromArray(SPECIES,mask, 0));
+
+                var s = sum.toDoubleArray();
+                var r = s[0] + s[1];
+                ///sum = sum.add(sum);
             /*System.err.println("CHICHON");
             System.out.println(sum);
             System.err.println("JEANMI");
             System.err.println(Arrays.toString(outv));*/
 
-            // to double should only be done on the first element
-            //outv[i] = sum.toDoubleArray()[0];
-            outv[i] = r;
+                // to double should only be done on the first element
+                //outv[i] = sum.toDoubleArray()[0];
+                outv[i] = r;
+            });
+
         });
     }
 
     private static void mult_Atv(double[] v, double[] outv, int n)
     {
-        var list = new int[n];
+        IntStream.range(0, n).forEach( i -> {
+            pool.execute(() -> {
+                var sum = DoubleVector.zero(SPECIES);
 
-        // make the array ascending
-        for (int i = 0; i < n ; i++) {
-            list[i] = i;
-        }
-
-        Arrays.stream(list).parallel().forEach( i -> {
-            var sum = DoubleVector.zero(SPECIES);
-
-            for (var j = 0; j < n; j += 2)
-            {
-                var b = DoubleVector.fromArray(SPECIES, v, j);
-                // needless array
-                var tmp = new double[2];
-                tmp[0] = A(j, i);
-                tmp[1] = A(j + 1, i);
-                var a = DoubleVector.fromArray(SPECIES, tmp, 0);
+                for (var j = 0; j < n; j += 2) {
+                    var b = DoubleVector.fromArray(SPECIES, v, j);
+                    // needless array
+                    var tmp = new double[2];
+                    tmp[0] = A(j, i);
+                    tmp[1] = A(j + 1, i);
+                    var a = DoubleVector.fromArray(SPECIES, tmp, 0);
 
 
-                sum = sum.add(b.div(a));
-            }
+                    sum = sum.add(b.div(a));
+                }
 
-            // should be horizontal
-            //sum = sum.add(sum);
-            var mask = new boolean[2];
-            Arrays.fill(mask, true);
-            var r = sum.reduceLanes(ADD, VectorMask.fromArray(SPECIES,mask, 0));
-            //var lel = sum.toDoubleArray();
-            //var r = lel[0] + lel[1];
+                // should be horizontal
+                //sum = sum.add(sum);
+                //var mask = new boolean[2];
+                //Arrays.fill(mask, true);
+                //var r = sum.reduceLanes(ADD, VectorMask.fromArray(SPECIES,mask, 0));
+                var s = sum.toDoubleArray();
+                var r = s[0] + s[1];
 
-            // to double should only be done on the first element
-            //outv[i] = sum.toDoubleArray()[0];
-            outv[i] = r;
+                //var lel = sum.toDoubleArray();
+                //var r = lel[0] + lel[1];
+
+                // to double should only be done on the first element
+                //outv[i] = sum.toDoubleArray()[0];
+                outv[i] = r;
+            });
         });
     }
 
